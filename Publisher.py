81408b7266f6d30c1483037b653e76c23dd73df1
Armon <abarakchi@ucsd.edu>
2024-04-03 13:38:41 -0700

    challenge 1 complete

--- Publisher.py
+++ Publisher.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as paho
 from paho import mqtt
 import time
+import random
 
 client1 = paho.Client(callback_api_version=paho.CallbackAPIVersion.VERSION1, client_id="publisher1", userdata=None, protocol=paho.MQTTv5)
 client2 = paho.Client(callback_api_version=paho.CallbackAPIVersion.VERSION1, client_id="publisher2", userdata=None, protocol=paho.MQTTv5)
@@ -20,6 +21,8 @@ client2.connect("5e411a7e06134539ac30e0ebb5aff733.s1.eu.hivemq.cloud", 8883)
 
 while True:
     time.sleep(3)
-    client1.publish("challenge1/cool", payload="hot", qos=1)
-    client2.publish("challenge1/beans", payload="hot", qos=1)
+    random_number1 = random.randint(1, 100)
+    random_number2 = random.randint(1, 100)
+    client1.publish("challenge1/cool", payload=random_number1, qos=1)
+    client2.publish("challenge1/beans", payload=random_number2, qos=1)
 
--- Subscriber.py
+++ Subscriber.py
@@ -1,12 +1,27 @@
 import paho.mqtt.client as paho
 from paho import mqtt
 
+def on_message(client, userdata, msg):
+    """
+        Prints a mqtt message to stdout ( used as callback for subscribe )
+        :param client: the client itself
+        :param userdata: userdata is set when initiating the client, here it is userdata=None
+        :param msg: the message with topic and payload
+    """
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+
+
 receiver = paho.Client(callback_api_version=paho.CallbackAPIVersion.VERSION1, client_id="receiver", userdata=None, protocol=paho.MQTTv5)
 
 receiver.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
 receiver.username_pw_set("beans", "Beans4Life")
+receiver.on_message = on_message
 
 receiver.connect("5e411a7e06134539ac30e0ebb5aff733.s1.eu.hivemq.cloud", 8883)
 
 # subscribe to all topics of encyclopedia by using the wildcard "#"
 receiver.subscribe("challenge1/#", qos=1)
+
+receiver.loop_forever()
+
